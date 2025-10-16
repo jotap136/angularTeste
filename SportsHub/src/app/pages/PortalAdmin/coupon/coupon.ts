2f824709832f4f989e8fef29545f95c857c4f622
giovanivvasconcelos <giovani.vvasconcelos@senacsp.edu.br>
2025-10-15 22:01:07 -0300

    First Code

---
+++ SportsHub/src/app/pages/PortalAdmin/coupon/coupon.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coupon',
+  imports: [],
+  templateUrl: './coupon.html',
+  styleUrl: './coupon.css'
+})
+export class Coupon {
+
+}
---
+++ SportsHub/src/app/pages/PortalAdmin/products/products.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-products',
+  imports: [],
+  templateUrl: './products.html',
+  styleUrl: './products.css'
+})
+export class Products {
+
+}
