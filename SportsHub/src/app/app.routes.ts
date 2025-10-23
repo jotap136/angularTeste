2545e052d6d9580ec322da8459f75fac3cf234bd
jotap136 <joaopcsantos136@gmail.com>
2025-10-23 17:12:58 -0300

    Primeiro commit no meu repositório

--- SportsHub/src/app/app.routes.ts
+++ SportsHub/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Products } from './pages/PortalAdmin/products/products'
 
-export const routes: Routes = [];
+export const routes: Routes = [ {
+    path: 'PortalAdmin/products',
+    component: Products,
+    title: 'produtos'
+} ];
